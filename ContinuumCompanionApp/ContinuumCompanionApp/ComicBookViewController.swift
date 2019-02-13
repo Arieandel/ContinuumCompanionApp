6f8269526ef5316cd087693f0ea0c842be601ae1
Ariel Andelt <>
2019-02-13 16:16:19 -0500

    Added comicbookviewcontroller for a test

---
+++ ContinuumCompanionApp/ContinuumCompanionApp/ComicBookViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ComicBookViewController.swift
+//  ContinuumCompanionApp
+//
+//  Created by Ariel Andelt on 2019-02-13.
+//  Copyright © 2019 Ariel Andelt. All rights reserved.
+//
+
+import Foundation
+
+
+class ComicBookViewController {
+    
+    
+}
