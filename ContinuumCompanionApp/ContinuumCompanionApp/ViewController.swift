51c346ecb19b5ac546b268a803800490d261b45c
Ariel Andelt <ariel@aandcorp.com>
2019-02-13 16:06:39 -0500

    Adding Project

---
+++ ContinuumCompanionApp/ContinuumCompanionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContinuumCompanionApp
+//
+//  Created by Ariel Andelt on 2019-02-13.
+//  Copyright © 2019 Ariel Andelt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
